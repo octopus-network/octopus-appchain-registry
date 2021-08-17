bbacb65551b512b8a02fa78f7b50253e164d7d29
Rivers Yang <rivers.yang@icloud.com>
2021-08-17 21:07:37 +0800

    Add implementation of 'registry_status'.

--- appchain-registry/src/registry_status.rs
+++ appchain-registry/src/registry_status.rs
@@ -17,3 +17,48 @@ pub trait RegistryStatus {
     /// Get downvote deposit of the caller for a certain appchain
     fn get_downvote_deposit_for(&self, appchain_id: AppchainId) -> Balance;
 }
+
+impl RegistryStatus for AppchainRegistry {
+    fn get_appchains_with_state_of(
+        &self,
+        appchain_state: Option<AppchainState>,
+    ) -> Vec<AppchainStatus> {
+        let mut results: Vec<AppchainStatus> = Vec::new();
+        let ids = self.appchain_basedatas.keys().collect::<Vec<String>>();
+        for id in ids {
+            let appchain_basedata = self.get_appchain_basedata(&id);
+            match appchain_state {
+                Some(ref state) => if appchain_basedata.state().eq(state) {
+                    results.push(appchain_basedata.status());
+                }
+                None => results.push(appchain_basedata.status()),
+            }
+        }
+        results
+    }
+
+    fn get_appchain_status_of(&self, appchain_id: AppchainId) -> AppchainStatus {
+        let appchain_basedata = self.get_appchain_basedata(&appchain_id);
+        appchain_basedata.status()
+    }
+
+    fn get_upvote_deposit_for(&self, appchain_id: AppchainId) -> Balance {
+        match self
+            .upvote_deposits
+            .get(&(appchain_id, env::predecessor_account_id()))
+        {
+            Some(value) => value,
+            None => 0,
+        }
+    }
+
+    fn get_downvote_deposit_for(&self, appchain_id: AppchainId) -> Balance {
+        match self
+            .downvote_deposits
+            .get(&(appchain_id, env::predecessor_account_id()))
+        {
+            Some(value) => value,
+            None => 0,
+        }
+    }
+}
