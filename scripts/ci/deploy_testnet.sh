89cd84cdb977992a1e62e27ef850fda98493c2e9
Rivers Yang <rivers.yang@icloud.com>
2023-02-14 14:55:47 +0800

    Add scripts for auto-deployment by github workflow.

---
+++ scripts/ci/deploy_testnet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+#
+export NEAR_ENV=testnet
+export REGISTRY_ACCOUNT_ID=registry.test_oct.testnet
+#
+near deploy --accountId $REGISTRY_ACCOUNT_ID --initFunction 'migrate_state' --initArgs '{}' --wasmFile res/appchain_registry.wasm
+#
+# view functions
+#
+near view $REGISTRY_ACCOUNT_ID version
+near view $REGISTRY_ACCOUNT_ID get_owner
+near view $REGISTRY_ACCOUNT_ID get_appchain_ids
+near view $REGISTRY_ACCOUNT_ID get_registry_roles
+near view $REGISTRY_ACCOUNT_ID get_registry_settings
+near view $REGISTRY_ACCOUNT_ID get_appchains_with_state_of '{"appchain_state":null,"page_number":1,"page_size":50,"sorting_field":"AppchainId","sorting_order":"Ascending"}'
