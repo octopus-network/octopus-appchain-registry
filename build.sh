16f10ea606fe342ee6854e59a5dc9b0918273b9b
Rivers Yang <rivers.yang@icloud.com>
2023-03-17 09:57:15 +0800

    Add `contract-builder`.

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cargo fmt --all
 RUSTFLAGS='-C link-arg=-s' cargo build --all --target wasm32-unknown-unknown --release
-if [ ! -d "out" ]; then
-    mkdir -p "out"
-fi
-if [ ! -d "res" ]; then
-    mkdir -p "res"
-fi
+
+mkdir -p out
+mkdir -p res
+
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
 cp target/wasm32-unknown-unknown/release/appchain_registry.wasm ./out/main.wasm
 
